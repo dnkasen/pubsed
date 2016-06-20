0a6dd865707d75d91942c2aa312ab3d7df877630
Daniel Kasen <kasen@berkeley.edu>
2016-06-20 13:00:52 -0700

    fixed up homologous expansion test compare scripts

--- tests/homologous_expansion/compare.py
+++ tests/homologous_expansion/compare.py
@@ -13,13 +13,13 @@ def compare(pdf):
  	temp_0 = 1.0e4
 	vmax = 0.5e9
 	r0   = texp*vmax*3600.0*24.
-	x,y = np.loadtxt('ray_00001',unpack=1,usecols=[0,4])
+	x,y = np.loadtxt('ray_00001.dat',unpack=1,usecols=[0,4])
 	py.plot(x/r0,y,color=cols[0])
-	x,y = np.loadtxt('ray_00010',unpack=1,usecols=[0,4])
+	x,y = np.loadtxt('ray_00010.dat',unpack=1,usecols=[0,4])
 	py.plot(x/r0,y,color=cols[1])
-	x,y = np.loadtxt('ray_00020',unpack=1,usecols=[0,4])
+	x,y = np.loadtxt('ray_00020.dat',unpack=1,usecols=[0,4])
 	py.plot(x/r0,y,color=cols[2])
-	x,y = np.loadtxt('ray_00030',unpack=1,usecols=[0,4])
+	x,y = np.loadtxt('ray_00030.dat',unpack=1,usecols=[0,4])
 	py.plot(x/r0,y,color=cols[3])
 	
 
@@ -29,8 +29,8 @@ def compare(pdf):
 		if (x[i] < r0): y[i] = temp_0
 
 	for i in range(0,4):
-		xn = x*(texp + i*10.0)/texp
-		yn = y*texp/(texp + i*10.0)
+		xn = x*(texp + (i)*10.0 + 0.5)/texp
+		yn = y*texp/(texp + (i)*10.0 + 0.5)
 		py.plot(xn/r0,yn,'--',color=cols[i])
 
 	py.xlim(0,3)
@@ -44,16 +44,16 @@ def compare(pdf):
 	t = []
 	r = []
 
-	for i in range(1,99):
+	for i in range(1,62):
 		if (i < 10): name = "ray_0000" + str(i)
 		else: name = "ray_000" + str(i)
+		name = name + '.dat'
 		x,y = np.loadtxt(name,unpack=1,usecols=[0,4])
 		tn = max(x)/1e9/3600.0/24.0
 		t.append(tn)
 		r.append(np.average(y[0:40]))
 
 
-	print t,r
 	py.plot(t,r,'o',markersize=8,markerfacecolor='none')
 	x = np.arange(20,100,0.1)
 	py.plot(x,temp_0*(texp/x),linewidth=2)
@@ -70,4 +70,4 @@ def compare(pdf):
 		py.show()
 		j = raw_input('press>')
 
-compare('')
+if __name__=='__main__': compare('')
