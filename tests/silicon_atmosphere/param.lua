580a9da12d0839b5e1aca62fa8b6a89d024a3308
Daniel Kasen <kasen@berkeley.edu>
2016-06-25 00:01:58 -0700

    more fixes to silicon atmosphere test

--- tests/silicon_atmosphere/param.lua
+++ tests/silicon_atmosphere/param.lua
@@ -11,16 +11,16 @@ core_radius      = 1e9*10*days
 core_luminosity  = 1e42
 core_temperature = 0.6e4
 
-particles_step_size = 0.1
+particles_step_size = 0.3
 
-transport_nu_grid   = {0.5e14,1.0e15,1e12}
+transport_nu_grid   = {0.5e14,1.0e15,0.001,1}
 spectrum_nu_grid   = transport_nu_grid
 
 opacity_epsilon  = 0.0
 opacity_electron_scattering = 0
 opacity_line_expansion      = 0
-opacity_lines               = 1
-line_velocity_width         = 1e8
+opacity_bound_bound         = 1
+line_velocity_width         = 1e7
 
 transport_steady_iterate = 1
 transport_radiative_equilibrium = 1
