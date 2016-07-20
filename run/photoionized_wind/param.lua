19cf6c3b9de89edbad228e933385f6dfe4f00a07
Daniel Kasen <kasen@127.0.0.1>
2016-07-20 01:33:23 -0700

    small change to param file

--- run/photoionized_wind/param.lua
+++ run/photoionized_wind/param.lua
@@ -14,6 +14,8 @@ core_luminosity       = 1e37
 core_temperature      = 40000.0
 core_radius           = 1e15
 core_n_emit           = 2e6
+core_spectrum_file    = ""
+
 
 -- output spectrum
 spectrum_nu_grid   = {1e14,5e15,0.00005,1}  -- frequency grid
