6333e54fe0ba004c90b2d5de55ee4031987c584a
Daniel Kasen <kasen@127.0.0.1>
2017-06-06 17:35:26 -0700

    changed cdf_array to be templated - can be any type

--- src/transport/cdf_array.h
+++ src/transport/cdf_array.h
@@ -12,29 +12,99 @@
 // the CDF value at locate_array's "min" is assumed to be 0
 //**********************************************************
 
-class cdf_array
+template < class T> class cdf_array
 {
 
 private:
   
-  std::vector<double> y;
+  std::vector<T> y;
   
 public:
 
-  double N;
   void resize(const int n)  {y.resize(n); }
 
-  double get(const int i)const             {return y[i];}   // Get local CDF value
-  void   set(const int i, const double f)  {y[i] = f;}      // Set cell CDF value 
 
-  void   set_value(const int i, const double f);     // set the actual (not CDF) value
-  double get_value(const int i) const;               // Get the actual (not CDF) value
- 
-  void normalize();         // normalize the cdf, so that final value = 1. Sets N.
-  int  sample(const double z) const;    // sample from the CDF, when passed a random #
-  void print() const;             
-  void wipe();
-  int size() const;
+  //------------------------------------------------------
+  // return the CDF value at index i
+  //------------------------------------------------------
+  T get(const int i) const 
+    {return y[i];}   
+  
+  //------------------------------------------------------
+  // set the CDF value at index i
+  //------------------------------------------------------
+  void   set(const int i, T f)  {y[i] = f;}      
+
+  //------------------------------------------------------
+  // return the actual y value, not the integrated CDF
+  //------------------------------------------------------
+  T get_value(const int i) const
+  {
+    if (i==0) return y[0];
+    else return (y[i] - y[i-1]);  
+  }
+
+  //------------------------------------------------------
+  // set the actual y value, not the integrated
+  // must be called in order
+  //------------------------------------------------------
+  void set_value(const int i, T f)   
+  {
+  if (i==0) y[0] = f;
+  else y[i] = y[i-1] + f;
+  }
+
+//------------------------------------------------------
+// Normalize such that the last entry is 1.0
+//------------------------------------------------------
+void normalize() 
+{
+  // check for zero array, set to all constant
+  if (y.back() == 0) y.assign(y.size(),1.0);
+
+  // normalize to end = 1.0
+  double N = y.back();
+  for (int i=0;i<y.size();i++)   y[i] /= N;
+}
+
+
+//---------------------------------------------------------
+// Sample the probability distribution using binary search.
+// Pass a random number betwen 0 and 1.  
+// Returns the index of the first value larger than yval
+// if larger than largest element, returns size
+//---------------------------------------------------------
+int sample(const double yval) const
+{
+  if (y.size() == 1) return 0;
+  return upper_bound(y.begin(), y.end(), yval) - y.begin();
+}
+
+
+//------------------------------------------------------
+// Simple printout
+//------------------------------------------------------
+void print() const{
+  for (int i=0;i<y.size();i++) 
+    printf("%5d %10.4e %10.4e\n",i,get_value(i),y[i]);
+}
+  
+//------------------------------------------------------
+// Clear the arrays
+//------------------------------------------------------
+void wipe()
+{
+  y.assign(y.size(), 0.0);
+}
+  
+//------------------------------------------------------------
+// just returning the size of the array
+//------------------------------------------------------------
+int size() const
+{
+  return y.size();
+}
+
 
 };
 
