77da5f96cd81f654cf5762cfd934f0cb2a5e8878
Daniel Kasen <kasen@berkeley.edu>
2017-08-30 01:28:51 -0700

    minor change to opacity example param file

--- examples/calculate_opacities/param_nu.lua
+++ examples/calculate_opacities/param_nu.lua
@@ -1,15 +1,13 @@
 -- Parameter file to plot opacity as a function of wavelength
 
-sedona_home = os.getenv("SEDONA_HOME")
-defaults_file = sedona_home.."/defaults/snopac_defaults.lua"
-
-data_atomic_file     = sedona_home.."/data/cmfgen_atomdata.hdf5"
+sedona_home       = os.getenv("SEDONA_HOME")
+defaults_file     = sedona_home.."/defaults/snopac_defaults.lua"
+data_atomic_file  = sedona_home.."/data/cmfgen_atomdata.hdf5"
 
 -- set this to somefilename output opacity as a function of wavelength (Angs)
 output_wavelength_file = "line_opac.dat"
 -- set this to somefilename output gas state (density, ionization, etc...)
 output_gas_state   = "gas_state.dat"
-
 
 -- list of isotopes to use: format = Z.A
 -- with Z = atomic number, A = atomic weight
@@ -19,17 +17,16 @@ elements_A  = {56}
 mass_fractions = {1.0}
 
 -- frequency grid to do calculations on (in Hz)
-nu1 = 3e10/(1e4*1e-8) --0.5e15
-nu2 = 3e10/(990*1e-8) -- 0.51e15
-line_velocity_width   = 1e7
-beta                  = 0.01
-transport_nu_grid     = {nu1,nu2,beta/1.0,1}
+nu1 = 3e10/(1e4*1e-8) 
+nu2 = 3e10/(990*1e-8) 
+-- logarthimic frequency spacing by 0.01
+transport_nu_grid     = {nu1,nu2,0.01,1}
 
-
+-- temperature, density, and time
 days = 3600.0*24.0
 density     =  {-13}
 temperature =  {6e3}
-time        =     20*days
+time        =  20*days
 
 -- opacities to use
 opacity_electron_scattering = 0
