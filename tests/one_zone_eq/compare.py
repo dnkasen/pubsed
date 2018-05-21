bd5aa06e54cf3723fd22bcf0b608255b968bc765
Nathaniel Roth <nathanielroth1@gmail.com>
2018-05-21 15:22:23 -0700

    More udpates to one_zone_eq test. Now using steady_iterate again, rather than explicit time stepping. Corrected the plotting of the level populations to use newer output format. Updated the plotting file so that numpy and matplotlib pyplot are loaded separately. Unphysical spikes still show up in the radiation field, but these should get better at higher nu resolution.

--- tests/one_zone_eq/compare.py
+++ tests/one_zone_eq/compare.py
@@ -1,5 +1,6 @@
 import os
-import pylab as py
+import numpy as np
+import matplotlib.pyplot as plt
 import h5py
 
 
@@ -8,70 +9,69 @@ def compare(pdf):
     ###########################################
     # compare the output
     ###########################################
-    py.clf()
-
+    plt.clf()
 
     h   = 6.6260755e-27    # planck's constant (ergs-s)
     c   = 2.99792458e10    # speed of light (cm/s)
     k   = 1.380658e-16     # boltzmann constant (ergs/K)
     sb  = 5.6704e-5        # stefan boltzman constant (ergs cm^-2 s^-1 K^-4) 
     pi  = 3.14159          # just pi
-    py.ion()
+    plt.ion()
     
     # compare spectrum 
-    py.clf()
+    plt.clf()
     fin = h5py.File('plt_00001.h5','r')
-    nu = py.array(fin['nu'])
-    Jnu = py.array(fin['zonedata/0/Jnu'])
-    opac = py.array(fin['zonedata/0/opacity'])
-    emis = py.array(fin['zonedata/0/emissivity'])
-    Snu = py.nan_to_num(emis/opac)
+    nu = np.array(fin['nu'])
+    Jnu = np.array(fin['zonedata/0/Jnu'])
+    opac = np.array(fin['zonedata/0/opacity'])
+    emis = np.array(fin['zonedata/0/emissivity'])
+    Snu = np.nan_to_num(emis/opac)
     fin.close()
-    py.plot(nu,Jnu,'o')
+    plt.plot(nu,Jnu,'o')
 
     # blackbody spectrum
     T = 1e5
-    f = 2.0*h*nu**2.0/c**2/(py.exp(h*nu/k/T) - 1)*nu
-    py.plot(nu,f,color='red',linewidth=2)
+    f = 2.0*h*np.power(nu, 2. * np.ones_like(nu))/c**2/(np.exp(np.minimum(h*nu/k/T,35. * np.ones_like(nu))) - 1) * nu # last factor of nu added here to avoid overflow
+    plt.plot(nu,f,color='red',linewidth=2)
     
-    py.legend(['sedona','analytic blackbody'])
-    py.title('one zone eq test: radiation field')
-    py.xlabel('frequency (Hz)')
-    py.ylabel('Flux')
-    py.yscale('log')
-    py.xscale('log')
-    py.xlim(1e14,5e16)
-    py.ylim(1e-4,5)
+    plt.legend(['sedona','analytic blackbody'])
+    plt.title('one zone eq test: radiation field')
+    plt.xlabel('frequency (Hz)')
+    plt.ylabel('Flux')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlim(1e14,5e16)
+    plt.ylim(1e-4,5)
 
     
     if (pdf != ''): pdf.savefig()
     else:
-        py.show()
+        plt.show()
         j = raw_input()
 
 
 
-    py.clf()
+    plt.clf()
 
-    fin = h5py.File('levels_00001.h5','r')
-    b = py.array(fin['zone_0/Z_1/level_departure'])
-    py.plot(b,'o',color='black')
-    py.plot([0,len(b)+1],[1,1],color='black')
-    py.title('one zone eq test: NLTE departure coefficients')
-    py.xlabel('level #',size=15)
-    py.ylabel('departure coefficient',size=15)
+    fin = h5py.File('plt_00001.h5','r')
+    b = np.array(fin['zonedata/0/Z_1/level_departure'])
+    plt.plot(b,'o',color='black')
+    plt.plot([0,len(b)+1],[1,1],color='black')
+    plt.title('one zone eq test: NLTE departure coefficients')
+    plt.xlabel('level #',size=15)
+    plt.ylabel('departure coefficient',size=15)
 
     for i in range(len(b)):
         if (b[i] > 1.2 or b[i] < 0.8):
-            py.plot([i,i],[-1,10],color='red')
+            plt.plot([i,i],[-1,10],color='red')
 
-    py.ylim(0.8,1.2)
+    plt.ylim(0.8,1.2)
 
 
     fin.close()
     if (pdf != ''): pdf.savefig()
     else:
-        py.show()
+        plt.show()
         j = raw_input()
 
 
--- tests/one_zone_eq/param.lua
+++ tests/one_zone_eq/param.lua
@@ -2,19 +2,11 @@ defaults_file = "../../defaults/sedona_defaults.lua"
 model_file    = "onezone.mod"       
 data_atomic_file     = "../../data/chianti_atomdata.hdf5" -- H_3lev_plusC_atomdata.hdf5"
 
-transport_nu_grid  = {3.5e13,3.e17,0.01,1}  -- frequency grid
-transport_steady_iterate         = 0
+transport_nu_grid  = {3.5e13,3.e17,0.005,1}  -- frequency grid
+transport_steady_iterate         = 1
 transport_radiative_equilibrium  = 0
 transport_boundary_in_reflect    = 1
 transport_boundary_out_reflect   = 1
-
-t0 = 1e4
-tstep_max_steps  = 6
-tstep_time_start = 0.0
-tstep_time_stop = 3 * t0
-tstep_max_dt     = t0
-tstep_min_dt     = t0
-output_write_times  = t0
 
 -- inner source emission
 particles_max_total  = 1e7
