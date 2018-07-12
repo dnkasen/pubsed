ca56babb82e5223607bdc67d37916ba816368037
Nathaniel Roth <nathanielroth1@gmail.com>
2018-07-11 20:48:25 -0400

    Fixing param file for one zone solar elements NLTE test

--- tests/one_zone_NLTE/optically_thin_solar/param.lua
+++ tests/one_zone_NLTE/optically_thin_solar/param.lua
@@ -1,10 +1,9 @@
 sedona_home   = os.getenv('SEDONA_HOME')
 defaults_file    = sedona_home.."/defaults/sedona_defaults.lua"
 data_atomic_file = sedona_home.."/data/atom_data.hdf5"
---data_atomic_file = "./atom_data.hdf5"
 model_file    = "onezone.mod"       
 
-transport_nu_grid  = {3.5e13,3.e17,0.002,1}  -- frequency grid
+transport_nu_grid  = {3.5e13,3.e17,0.005,1}  -- frequency grid
 transport_steady_iterate         = 0
 transport_radiative_equilibrium  = 1
 transport_boundary_in_reflect    = 1
@@ -22,24 +21,22 @@ particles_max_total  = 1e7
 core_n_emit          = 0
 core_radius          = 1.0e14
 particles_n_emit_thermal  = 0
-particles_n_initialize  = 5e7
+particles_n_initialize  = 1e7
 
 -- output spectrum
 spectrum_nu_grid    = transport_nu_grid
 output_write_atomic_levels = 1
 output_write_radiation = 1
 
-output_write_mass_fractions = 1
-
 -- opacity information
 opacity_grey_opacity  = 0
 
 opacity_use_nlte      = 1
 opacity_atoms_in_nlte = {1,2,6,7,8,26}
-opacity_bound_bound   = 0
+opacity_bound_bound   = 1
 opacity_bound_free    = 1
-opacity_free_free     = 0
-opacity_electron_scattering = 0
+opacity_free_free     = 1
+opacity_electron_scattering = 1
 line_velocity_width   = 3e8
 
 
