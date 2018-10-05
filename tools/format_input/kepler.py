82415c0607dac38d6c80f926c64592b822d2a923
Chelsea Harris <chelseaharris@berkeley.edu>
2018-10-05 14:56:32 -0400

    can now read, convert, and write Kepler files. Untested on real simulation

--- tools/format_input/kepler.py
+++ tools/format_input/kepler.py
@@ -4,25 +4,273 @@ Creation Date: Oct 1, 2018
 Purpose: Convert Kepler output into a Sedona input model for light curve and spectra creation.
 """
 
-import argparse # Tool for parsing command line arguments
-import numpy
+import numpy as np
 
-def read_original(rfn):
+# Maps between the way Kepler refers to isotopes (e.g., 'h1') and the way Sedona does (e.g., '1.1')
+Sed2Kep = {'1.1':'h1', '2.4': 'he4', '6.12':'c12', '7.14': 'n14', '8.16':'o16', '10.20':'ne20',
+           '12.24':'mg24', '14.28':'si28', '16.32':'s32', '18.36':'ar36', '20.40':'ca40', 
+           '22.44':'ti44', '24.48':'cr48', '26.52':'fe52', '26.54':'fe54', '26.56':'fe56', 
+           '27.56':'co56', '28.56':'ni56'}
+Kep2Sed = {}
+for key in Sed2Kep.keys():
+    Kep2Sed[Sed2Kep[key]] = key
+# These species are in Kepler but not in Sedona (or for some other reason 
+# it is desired to lump them in with another element).
+# In the conversion process they will be lumped with other elements
+Kep2Sed['nt1'] = '' # to be lumped with 1.1
+Kep2Sed['pn1'] = '' # ... 1.1
+Kep2Sed['he3'] = '' # ... 2.4
+Kep2Sed['fe54'] = '' # ... 26.52
+
+def read_original(rfn, verbose=False):
+    """
+    Reads a Kepler table into a dictionary. Kepler output tables are 
+    a difficult format to read in because there aren't an equal number of
+    columns per row, nor a specified header, and there can be multiple tables
+    on one row (in the "header")
+    INPUTS
+    rfn : path to the Kepler model (".link") file
+    """
+    if verbose: print("Reading file.")
+
+    # Because the file is all weird, I'm just opening it the non-numpy way
+    with open(rfn,'r') as rf:
+        all_lines = rf.read().split('\n')
+
+    # Separate the top header from the rest, and locate the "blocks" of data
+    mat = []
+    i_block_start = []
+    n = 0
+    main_header = ''
+    for line in all_lines:
+        cols = line.split()
+
+        if len(cols) == 0: 
+            continue
+
+        if cols[0] == 'zone': 
+            i_block_start.append(n)
+        elif cols[0] == 'total':
+            continue
+
+        if len(i_block_start)==0: main_header += line
+
+        mat.append(cols)
+
+        n+=1
+
+    # Add fake block start so we can do the next for-loops
+    i_block_start.append(len(all_lines))
+    # Put blocks into arrays, taking out the "zone" column
+    blocks = [ np.array(mat[i_block_start[0]+1: i_block_start[0+1]])[:,1:] ]
+    headers = [ mat[i_block_start[0]][1:] ]
+    # also take out mass column for blocks after the first one
+    blocks += [ np.array(mat[i_block_start[i]+1: i_block_start[i+1]])[:,2:]  for i in range(1,len(i_block_start)-1) ]
+    headers += [ mat[i][2:] for i in i_block_start[1:-1] ]
+
+    # Truncate the second block because its 'burn off' entries are counted as two columns! 
+    # And I don't need them
+    blocks [1] = blocks [1][:,:9]
+    headers[1] = headers[1][:9]
+
+    # Check that the blocks are OK in shape
+    for block in blocks[1:]:
+        if block.shape[0] - blocks[0].shape[0] != 0: 
+            print('It seems that the script is not identifying blocks correctly.')
+            return -1
+
+    # Merge the blocks into one table, same with the column headers
+
+    # populate the headers array
+    all_hdrs = []
+    species = []
+    for i, hdr_line in enumerate(headers):
+        all_hdrs += hdr_line
+        if i>=2:
+            species += hdr_line
+    all_hdrs = np.array(all_hdrs)
+    species = species[:-1]
+
+    # the data array
+    n_rows = blocks[0].shape[0]
+    n_cols = sum( [block.shape[1] for block in blocks] )
     
+    tab = np.zeros((n_rows, n_cols)) # each block is contributing a "zone" column
+    j0 = 0
+    for block in blocks:
+        try:
+            tab[:, j0:j0+block.shape[1]] = block.astype(float)
+        except ValueError:
+            for j in range(block.shape[1]):
+                try:
+                    tab[:, j0+j] = block[:,j].astype(float)
+                except ValueError:
+                    if verbose:
+                        print('Setting the {} column to zero because it isn\'t a float'.format(all_hdrs[j0+j]))
+                        print('  e.g., value of '+repr(block[0,j]))
+        j0 += block.shape[1]
 
+    # Make into a dictionary
+    Table = {}
+    for j in range(all_hdrs.size):
+        Table[all_hdrs[j]] = tab[:,j]
+    return Table, main_header, species
+
+
+def convert(Table, header, species, 
+            verbose=False):
+    """
+    This function takes the information from the Kepler table and generates
+    the table that Sedona needs.
+    It does not generate the header that Sedona needs, but does read (and return) 
+    the time.
+    Note that the 'nt1' and 'pn1' columns are lumped into '1.1' with 'nt1' multiplied
+    by 0.1,
+    the 54-Co mass fraction is set to 1e-10, 
+    and 52-Fe includes 'fe52' and 'fe54'    
+
+    INPUTS
+    Table  : [dict]  key, value pairs are column names and columns from the Kepler blocks
+                     (output of read_original())
+    header : [str]   the Kepler model file before the blocks start
+                     (output of read_original())
+    species: [str[]] list of the species to put into the Sedona model, written as Sedona
+                     types (e.g. '1.1')
+                     (output of read_original() or specified)
+    verbose: [bool]  whether to print messages
+
+    OUTPUTS
+    mat    : [np.ndarray(float)] the main body of the Sedona model
+    time   : [float]             model time (sec)
+    """
+
+    if verbose:
+        print('Converting data to Sedona version')
+
+    time = float(header.split()[10])
+    n_zones = len(Table[list(Table.keys())[0]])
+
+    # properties needed by sedona, in order
+    needed = ('velocity', 'density', 'temp')
+
+    # initialize the matrix that will be ouptut as the model
+    mat = np.zeros((n_zones, len(needed)+len(species)))
+
+    for i, col_name in enumerate(needed):
+        mat[:,i] = Table[col_name]
+
+
+    for i, s in enumerate(species):
+        # If the species list is coming from Kepler, some 
+        # elements will have been unknown
+        if s=='':
+            continue
+        elif s=='1.1':
+            # for some reason the conversion script I got from Stan 
+            # has 'nt1' being divided by 10 before being stored
+            x = Table['h1'] + Table['nt1']/10 + Table['pn1']
+        elif s=='2.4':
+            x = Table['he3'] + Table['he4']
+        elif s=='27.54':
+            x = 1e-10*np.ones(n_zones)
+        elif s=='26.52':
+            x = Table['fe52'] + Table['fe54']
+        else:
+            x = Table[Sed2Kep[s]]
+
+        mat[:, i+len(needed)] = x
+
+    return mat, time
+
+
+def overwrite_safety(wfn):
+    """
+    This function checks if wfn already exists, and re-names the
+    new file destination if so.
+    """
+    import glob
+    
+    exists = len(glob.glob(wfn))
+    if exists:
+        print('Will not write file '+wfn+' -- file exists!')
+        base = wfn
+        i=1
+        while exists:
+            wfn = base+'.'+str(i)
+            exists = len(glob.glob(wfn))
+            i+=1
+    print('Writing to '+wfn)
+
+    return wfn
+
+
+def write_new(wfn, time, species, sim_data, rfn='', 
+              grid_type='1D_sphere', hydro_type='SNR', overwrite=False):
+    """
+    A function to generate the Sedona header and write the sedona model file
+
+    INPUTS
+    wfn  : [str] path for new file
+    time : [float] time of model in seconds
+    species: [list(str)] species to include, in Sedona notation (e.g., '1.1')
+    sim_data: [np.ndarray(float)] main Sedona body (output of convert())
+    """
+    # Check that we have a write-file name/path:
+    if wfn=='':
+        if rfn=='':
+            wfn = './KeplerOut.mod'
+        else:
+            wfn = rfn.replace(rfn.split('.')[-1], 'mod')
+    # If safety is desired, check this name
+    if not overwrite:
+        wfn = overwrite_safety(wfn)
+
+    # Write the Sedona header
+    hdr = grid_type + '  ' + hydro_type + '\n'
+    hdr += '{:d} 0.0 {:.5e} {:d}\n'.format(sim_data.shape[0], time, len(species))
+    hdr += ' '.join(species)
+
+    # store it all!
+    np.savetxt(wfn, sim_data, header=hdr, comments='')
+    
+            
 
 def main(args):
+    # Read in the Kepler file
+    Table, header, species = read_original(args.rfn, verbose=args.verbose)
+    # Turn it into a data matrix for Sedona
+    spec2use = args.specs if len(args.specs)>0 else [ Kep2Sed[s] for s in species ]
+    mat, time = convert(Table, header, spec2use, verbose=args.verbose)
+    # Write to new file
+    write_new(args.wfn, time, spec2use, mat, 
+              rfn=args.rfn, grid_type=args.grid, hydro_type=args.hydro, overwrite=args.overwrite)
 
 
 
 if __name__=='__main__':
-    parser = argpase.ArgumentParser()
+    import argparse
+
+    parser = argparse.ArgumentParser()
     # Make arguments for this converter
     parser.add_argument('rfn', type=str, help='path to file')
 
-    parser.add_argument('time', type=float, help='time of the simulation in seconds')
+    #parser.add_argument('time', type=float, help='time of the simulation in seconds')
 
-    parser.add_argument('--wfn', type=str, help='path for desired output file')
+    parser.add_argument('--wfn', type=str, default='', help='path for desired output file')
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='(flag) print messages to screen during execution')
+    parser.add_argument('--specs', type=str, nargs='+', default=[], 
+                        help='list of species for sedona (default is all that are in the Kepler file) formatted like --specs = s1 s2 s3 ...')
+
+    parser.add_argument('--grid', type=str, default='1D_sphere',
+                        help='sedona grid parameter to use')
+
+    parser.add_argument('--hydro', type=str, default='SNR',
+                        help='sedona hydro parameter to use')
+
+    parser.add_argument('--overwrite', action='store_true', 
+                        help='(flag) throw caution to the wind when writing the new file')
 
     # Use the arguments to make the appropriate conversion
     args = parser.parse_args()
