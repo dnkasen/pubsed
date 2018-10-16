28b05820074773f027e7dd805054b7f331b64548
dnkasen <kasen@berkeley.edu>
2018-10-15 17:18:48 -0700

    Increased # of iterations of test toyIa spectrum by 1

--- tests/toy_type1a_supernova/1D_spectrum/param.lua
+++ tests/toy_type1a_supernova/1D_spectrum/param.lua
@@ -12,7 +12,7 @@ data_atomic_file   = sedona_home.."/data/cmfgen_levelcap100.hdf5"
 -- transport properites
 transport_nu_grid  = {0.8e14,1.0e16,0.001,1}  -- frequency grid
 transport_radiative_equilibrium  = 1
-transport_steady_iterate         = 4
+transport_steady_iterate         = 5
 
 -- output spectrum frequency grid
 spectrum_nu_grid   = {0.8e14,1.0e16,0.002,1}
@@ -21,7 +21,7 @@ spectrum_nu_grid   = {0.8e14,1.0e16,0.002,1}
 tstep_time_start = 20*3600.0*24.0
 
 -- radioactive particle emission
-particles_n_emit_radioactive = 1e5
+particles_n_emit_radioactive = 2e5
 particles_last_iter_pump     = 10
 
 -- opacity information
