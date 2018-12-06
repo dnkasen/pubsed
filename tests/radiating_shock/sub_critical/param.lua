c2520c5f69a2e1436427d0464dd326fac4c704f9
Daniel Kasen <kasen@berkeley.edu>
2018-12-06 15:34:28 -0800

    added radiating shock test problem

---
+++ tests/radiating_shock/sub_critical/param.lua
@@ -0,0 +1,27 @@
+sedona_home   = os.getenv('SEDONA_HOME')
+
+defaults_file    = sedona_home.."/defaults/sedona_defaults.lua"
+data_atomic_file = sedona_home.."/data/ASD_atomdata.hdf5"
+
+grid_type  = "grid_1D_sphere"    -- grid geometry; must match input model
+model_file = "../models/ensman_model.mod"        -- input model file
+
+hydro_module     = "1D_lagrangian"
+hydro_gamma_index = 5.0/3.0
+hydro_v_piston    = 2e5
+transport_module = "monte_carlo"
+
+particles_n_initialize    = 1000
+particles_n_emit_thermal  = 10000
+opacity_grey_opacity      = 0.4
+opacity_epsilon           = 1
+
+-- time stepping
+tstep_max_steps    = 200000
+tstep_time_stop    = 0.9e5
+
+output_write_plt_file_time        = 1e4
+
+
+
+
