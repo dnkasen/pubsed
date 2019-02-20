78886f0cf734918b98ba6afefd1327a9db800546
Daniel Kasen <kasen@berkeley.edu>
2019-02-20 15:40:34 -0800

    add read of param file name from command line

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,10 +11,16 @@ int main(int argc, char **argv)
     MPI_Init( &argc, &argv );
 #endif
 
-  // This runs the entire calculation
-  // see functions in SedonaClass.cpp
+  // get name of parameter file -- default is defined in sedona.h
+  std::string param_file = DEFAULT_PARAM_FILE_NAME;
+  if( argc > 1 ) param_file = std::string( argv[ 1 ] );
+
+  // This runs the entire sedona calculation using the
+  // passed parameter file name
+  // See the functions in SedonaClass.cpp for how
+  // it is all done
   SedonaClass sedona;
-  sedona.run("param.lua");
+  sedona.run(param_file);
 
 #ifdef MPI_PARALLEL
     MPI_Finalize();
