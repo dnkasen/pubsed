b2128fc1e4a14ec2f6f87a02e537ab94c7647aef
Daniel Kasen <kasen@berkeley.edu>
2019-07-17 15:54:29 -0700

    Add a new class that holds all atomic data

---
+++ src/opacity/AtomicData.cpp
@@ -0,0 +1,444 @@
+#include "AtomicData.h"
+#include "physical_constants.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include "hdf5.h"
+#include "hdf5_hl.h"
+
+namespace pc = physical_constants;
+
+
+//----------------------------------------------------------------
+// Initialize an atom by reading in the data from the hdf5 file
+// given by name "fname"
+//----------------------------------------------------------------
+AtomicData::AtomicData()
+{
+  // set all atoms to nothing
+  for (int i=0;i<=MAX_N_ATOMS;++i)
+  {
+    atomlist_[i].data_exists_ = false;
+    atomlist_[i].n_ions_   = 0;
+    atomlist_[i].n_levels_ = 0;
+    atomlist_[i].n_lines_  = 0;
+    atomlist_[i].fuzz_lines_.n_lines = 0;
+  }
+}
+
+AtomicData::~AtomicData()
+{
+}
+
+
+int AtomicData::initialize(std::string fname, locate_array ng)
+{
+  // copy over frequency grid
+  nu_grid_.copy(ng);
+
+  // copy over data file name
+  atom_datafile_ = fname;
+
+  // see if data file exists
+  std::ifstream f(fname.c_str());
+  if (!f.good())
+  {
+      std::cout << "# ERROR -- can't open atomic data file ";
+      std::cout << fname << std::endl;
+      return 1;
+  }
+  return 0;
+}
+
+
+
+
+//------------------------------------------------------------------------
+// Print out general stats on all atomic data read and stored
+//------------------------------------------------------------------------
+void AtomicData::print()
+{
+  std::cout << "#-------------------------------------------------\n";
+  std::cout << "# atomic data from: " << atom_datafile_ << "\n";
+  std::cout << "#--------------------------------------------------\n";
+  std::cout << "#  Z    n_ions  n_levels  n_lines  n_fuzz_lines\n";
+  std::cout << "#-------------------------------------------------\n";
+  for (int i=0;i<MAX_N_ATOMS;++i)
+  {
+    IndividualAtomData *atom = &(atomlist_[i]);
+    if (atom->data_exists_ == false) continue;
+    printf("# %3d   ",i);
+    int nfuzz = atom->get_n_fuzz_lines();
+    printf(" %4d %8d  %8d  %8d",atom->n_ions_,atom->n_levels_,atom->n_lines_,nfuzz);
+    printf("\n");
+  }
+  std::cout << "#-------------------------------------------------\n";
+}
+
+//------------------------------------------------------------------------
+// Print detailed stats for a certain element
+//------------------------------------------------------------------------
+void AtomicData::print_detailed(int z)
+{
+
+  IndividualAtomData *atom = &(atomlist_[z]);
+  if (atom->data_exists_ == false)
+  {
+      std::cout << "# Can't print data for element " << z;
+      std::cout << " ; doesn't exist\n";
+      return;
+  }
+
+  std::cout << "#-------------------------------------------------\n";
+  std::cout << "# atomic data from: " << atom_datafile_ << "\n";
+  std::cout << "#--------------------------------------------------\n";
+  std::cout << "#  Z    n_ions  n_levels  n_lines  n_fuzz_lines\n";
+  std::cout << "#-------------------------------------------------\n";
+  printf("# %3d   ",z);
+  int nfuzz = atom->get_n_fuzz_lines();
+  printf(" %4d %8d  %8d  %8d",atom->n_ions_,atom->n_levels_,atom->n_lines_,nfuzz);
+  printf("\n");
+
+ for (int i=0;i<atom->n_ions_;++i)
+ {
+    std::cout << "#-------------------------------------------------" << std::endl;
+    std::cout << "ion stage       = " << atom->ions_[i].stage << std::endl;
+    std::cout << "ionization chi  = " << atom->ions_[i].chi << " eV" << std::endl;
+    std::cout << "ground level id = " << atom->ions_[i].ground << std::endl;
+    std::cout << "#------------------------------------------------" << std::endl;;
+    std::cout << std::endl;
+  }
+}
+
+
+//------------------------------------------------------------------------
+// Read all atomic data for species with atomic number Z
+//------------------------------------------------------------------------
+int AtomicData::read_atomic_data(int z)
+{
+  // return if this is an invalid atom
+  if ((z < 1)||(z >= MAX_N_ATOMS))
+  {
+    std::cout << "ERROR: Atomic number " << z << " is not allowed!" << std::endl;
+    return 1;
+  }
+
+  // return if this atom has already been read
+  if (atomlist_[z].data_exists_)
+    return 2;
+
+  // open hdf5 file
+  herr_t status;
+  status = H5Eset_auto1(NULL, NULL);
+  hid_t file_id = H5Fopen (atom_datafile_.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+
+  // get element group name
+  char atomname[100];
+  sprintf(atomname,"%d/",z);
+  char dset[1000];
+
+  IndividualAtomData *atom = &(atomlist_[z]);
+
+  // check if this species is in the atom_datafile_
+  // if not -- just fill up with single level
+  status = H5Gget_objinfo (file_id, atomname, 0, NULL);
+  if (status != 0)
+  {
+      std::cout << "# ERROR: No atomic data for species ";
+      std::cout << z << " ; Filling with empty data\n";
+      atom->n_ions_   = 1;
+      atom->n_levels_ = 1;
+      atom->n_lines_  = 0;
+      atom->ions_ = new AtomicIon[1];
+      atom->ions_[0].chi    = 999999;
+      atom->ions_[0].ground = 0;
+      atom->ions_[0].stage  = 0;
+      atom->levels_ = new AtomicLevel[1];
+      atom->levels_[0].E  = 0;
+      atom->levels_[0].g  = 1;
+      atom->levels_[0].ic = -1;
+      atom->levels_[0].ion = 0;
+      return 1;
+  }
+  // default, no fuzzlines
+  // fuzz_lines.n_lines = 0;
+
+  atomlist_[z].data_exists_ = true;
+
+  //----------------------------------------
+  // read ion data
+  // ----------------------------------------
+  int n_ions;
+  status = H5LTget_attribute_int(file_id, atomname, "n_ions", &n_ions);
+  if (status != 0) return -1;
+  atom->ions_ = new AtomicIon[n_ions];
+  atom->n_ions_ = n_ions;
+
+  // read ionization potentials
+  sprintf(dset,"%s%s",atomname,"ion_chi");
+  double *ion_darr = new double[n_ions];
+  status = H5LTread_dataset_double(file_id,dset,ion_darr);
+  if (status != 0) return -1;
+  for (int i=0;i<n_ions;++i) atom->ions_[i].chi  = ion_darr[i];
+
+  // read ground state levels
+  sprintf(dset,"%s%s",atomname,"ion_ground");
+  int *ion_iarr = new int[n_ions];
+  status = H5LTread_dataset_int(file_id,dset,ion_iarr);
+  if (status != 0) return -1;
+  for (int i=0;i<n_ions;++i) atom->ions_[i].ground  = ion_iarr[i];
+
+  for (int i=0;i<n_ions;++i)
+    atom->ions_[i].stage  = i;
+
+  // clean up
+  delete[] ion_darr;
+  delete[] ion_iarr;
+
+  // ----------------------------------------
+  // read levels
+  // ----------------------------------------
+  // get number of levels
+  int n_levels;
+  status = H5LTget_attribute_int(file_id, atomname, "n_levels",&n_levels);
+  if (status != 0) return -1;
+  atom->levels_ = new AtomicLevel[n_levels];
+  atom->n_levels_ = n_levels;
+
+  // make space to read data
+  double *lev_darr = new double[n_levels];
+  int *lev_iarr = new int[n_levels];
+
+  // read level statistical weights
+  sprintf(dset,"%s%s",atomname,"level_g");
+  status = H5LTread_dataset_int(file_id,dset,lev_iarr);
+  for (int i=0;i<n_levels;++i) atom->levels_[i].g = lev_iarr[i];
+
+  // read level ionization stage
+  sprintf(dset,"%s%s",atomname,"level_i");
+  status = H5LTread_dataset_int(file_id,dset,lev_iarr);
+  for (int i=0;i<n_levels;++i) atom->levels_[i].ion = lev_iarr[i];
+
+  // read level excitation energy
+  sprintf(dset,"%s%s",atomname,"level_E");
+  status = H5LTread_dataset_double(file_id,dset,lev_darr);
+  for (int i=0;i<n_levels;++i) atom->levels_[i].E = lev_darr[i];
+
+  // set other parameters
+  for (int i=0;i<n_levels;++i)
+  {
+    atom->levels_[i].E_ion = atom->ions_[atom->levels_[i].ion].chi - atom->levels_[i].E;
+
+    // set 0 statistical weights to 1
+    if (atom->levels_[i].g == 0) atom->levels_[i].g = 1;
+
+    // find the level that this ionizes to (= -1 if none)
+    atom->levels_[i].ic = -1;
+    for (int j=0;j<n_ions;j++)
+      if (atom->ions_[j].stage == atom->levels_[i].ion + 1)
+      	atom->levels_[i].ic  = atom->ions_[j].ground;
+  }
+
+  // clean up
+  delete[] lev_iarr;
+  delete[] lev_darr;
+
+  // ----------------------------------------
+  // read lines
+  // ----------------------------------------
+
+  // get number of lines
+  int n_lines;
+  status = H5LTget_attribute_int(file_id, atomname, "n_lines",&n_lines);
+  if (status != 0) n_lines = 0;
+  atom->lines_ = new AtomicLine[n_lines];
+  atom->n_lines_ = n_lines;
+
+  if (n_lines > 0)
+  {
+    double *lin_darr = new double[n_lines];
+    int *lin_iarr    = new int[n_lines];
+
+    // read line lower levels
+    sprintf(dset,"%s%s",atomname,"line_l");
+    status = H5LTread_dataset_int(file_id,dset,lin_iarr);
+    if (status != 0) return -1;
+    for (int i=0;i<n_lines;++i) atom->lines_[i].ll = lin_iarr[i];
+
+    // read line upper levels
+    sprintf(dset,"%s%s",atomname,"line_u");
+    status = H5LTread_dataset_int(file_id,dset,lin_iarr);
+    for (int i=0;i<n_lines;++i) atom->lines_[i].lu = lin_iarr[i];
+
+    // read line Einstein A
+    sprintf(dset,"%s%s",atomname,"line_A");
+    status = H5LTread_dataset_double(file_id,dset,lin_darr);
+    for (int i=0;i<n_lines;++i) atom->lines_[i].A_ul = lin_darr[i];
+
+    delete[] lin_darr;
+    delete[] lin_iarr;
+  }
+
+  // set additional line properties
+  for (int i=0;i<n_lines;++i)
+  {
+    int ll = atom->lines_[i].ll;
+    int lu = atom->lines_[i].lu;
+
+    // get wavelength/frequency
+    double delta_E = atom->levels_[lu].E - atom->levels_[ll].E;
+    double nu      = delta_E*pc::ev_to_ergs/pc::h;
+    double lam   = pc::c/nu*pc::cm_to_angs;
+    atom->lines_[i].nu  = nu;
+
+    // set Einstein Coeficients
+    int gl = atom->levels_[ll].g;
+    int gu = atom->levels_[lu].g;
+    double A = atom->lines_[i].A_ul;
+    atom->lines_[i].B_ul = A*pc::c*pc::c/2.0/pc::h/nu/nu/nu;
+    atom->lines_[i].B_lu = atom->lines_[i].B_ul*gu/gl;
+
+    // set oscillator strength (see e.g., Rutten page 24)
+    double lam_cm = lam*pc::angs_to_cm;
+    atom->lines_[i].f_lu = lam_cm*lam_cm*A*gu/gl/(8*pc::pi*pc::sigma_tot);
+
+    // find index of bin in deal
+    atom->lines_[i].bin = nu_grid_.locate_within_bounds(nu);
+  }
+
+  // ----------------------------------------
+  // read photoionization cross-sections
+  // if not available, just use hydrogenic approx
+  // ----------------------------------------
+  int npts     = 100;
+  double fmax  = 10;
+  for (int i=0;i<n_levels;++i)
+  {
+     // set photoionization cross-section
+     double E_ion = atom->levels_[i].E_ion;
+     double E_max = E_ion*fmax;
+     double dE = (E_max - E_ion)/npts;
+     atom->levels_[i].s_photo.init(E_ion,E_max, dE);
+
+     // effective excitation quantum number
+     double E_ground = atom->ions_[atom->levels_[i].ion].chi;
+     double n_eff = pow(1 - (E_ground - E_ion)/E_ground,-0.5);
+     double s_fac = n_eff/(atom->levels_[i].ion+1)/(atom->levels_[i].ion + 1);
+
+    double nu_t = E_ion*pc::ev_to_ergs/pc::h;
+    int istart = 0; int istop = 0;
+    for (int k=0;k<nu_grid_.size();k++)
+    {
+      if (nu_grid_.center(k) > nu_t)
+        if (istart == 0) istart = k;
+
+      if (nu_grid_.center(k) > 10*nu_t)
+        if (istop == 0) istop = k;
+    }
+//    std::cout << "is " << i << " " << nu_t << " " << E_ion << "\n"; //istop - istart << " " << istop << "\n";
+
+     //verner data
+     // double V_Eth = 0.1360E+02;
+     // double V_E0  = 0.4298E+00;
+     // double V_s0  = 0.5475E+05;
+     // double V_ya  = 0.3288E+02;
+     // double V_P   = 0.2963E+01;
+     // double V_yw  = 0.0000E+00;
+
+     for (int j=0;j<npts;j++)
+     {
+        double E = atom->levels_[i].s_photo.x[j];
+        //double y = E/V_E0;
+        //double Fr = ((y-1)*(y-1) + V_yw*V_yw)*
+        double sigma = 6.3e-18*s_fac*pow(E/E_ion,-3.0); //2.5);
+        atom->levels_[i].s_photo.y[j] = sigma;
+     }
+  }
+
+   H5Fclose(file_id);
+   return 0;
+}
+
+
+//------------------------------------------------------------------------
+// Read a file constisting of (aka "fuzz") lines
+// Do so for any atom that has data already defined
+//------------------------------------------------------------------------
+int AtomicData::read_fuzzfile_data(std::string fname)
+{
+  int n_lines = 0;
+  for (int i=0;i<MAX_N_ATOMS;++i)
+  {
+    if (atomlist_[i].data_exists_)
+      n_lines += read_fuzzfile_data_for_atom(fname,i);
+  }
+  return n_lines;
+}
+
+
+//------------------------------------------------------------------------
+// Read a file constisting of (aka "fuzz") lines
+// for a signel atom
+//------------------------------------------------------------------------
+int AtomicData::read_fuzzfile_data_for_atom(std::string fname, int Z)
+{
+
+  // open hdf5 file
+  hid_t file_id = H5Fopen (fname.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+  herr_t status;
+
+  // get element group name
+  char atomname[100];
+  sprintf(atomname,"%d/",Z);
+  char dset[1000];
+
+  int nl;
+  status = H5LTget_attribute_int(file_id, atomname, "n_lines", &nl);
+  if (status != 0) return -1;
+
+  atomlist_[Z].fuzz_lines_.n_lines = nl;
+  atomlist_[Z].fuzz_lines_.nu.resize(nl);
+  atomlist_[Z].fuzz_lines_.gf.resize(nl);
+  atomlist_[Z].fuzz_lines_.El.resize(nl);
+  atomlist_[Z].fuzz_lines_.ion.resize(nl);
+  atomlist_[Z].fuzz_lines_.bin.resize(nl);
+
+  // read in arrays
+  double *darr = new double[nl];
+  int    *iarr = new int[nl];
+
+  // read line frequency
+  sprintf(dset,"%s%s",atomname,"nu");
+  status = H5LTread_dataset_double(file_id,dset,darr);
+  for (int i=0;i<nl;++i) atomlist_[Z].fuzz_lines_.nu[i] = darr[i];
+
+  // read line gf
+  sprintf(dset,"%s%s",atomname,"gf");
+  status = H5LTread_dataset_double(file_id,dset,darr);
+  for (int i=0;i<nl;++i) atomlist_[Z].fuzz_lines_.gf[i] = darr[i];
+
+  // read line lower level excitation energy
+ sprintf(dset,"%s%s",atomname,"El");
+ status = H5LTread_dataset_double(file_id,dset,darr);
+ for (int i=0;i<nl;++i) atomlist_[Z].fuzz_lines_.El[i] = darr[i];
+
+ // read line ionization state
+ sprintf(dset,"%s%s",atomname,"ion");
+ status = H5LTread_dataset_int(file_id,dset,iarr);
+ for (int i=0;i<nl;++i) atomlist_[Z].fuzz_lines_.ion[i] = iarr[i];
+
+ // get frequency bin of line
+ for (int i=0;i<nl;++i)
+ {
+   int ind = nu_grid_.locate(atomlist_[Z].fuzz_lines_.nu[i])-1;
+   if (ind < 0) ind = 0;
+   atomlist_[Z].fuzz_lines_.bin[i] = ind;
+ }
+
+  delete[] darr;
+  delete[] iarr;
+  H5Fclose(file_id);
+
+  return nl;
+}
---
+++ src/opacity/AtomicData.h
@@ -0,0 +1,185 @@
+#ifndef _ATOMIC_DATA_H
+#define _ATOMiC_DATA_H 1
+
+#define MAX_N_ATOMS 120
+
+#include <string>
+#include <vector>
+#include "sedona.h"
+#include "xy_array.h"
+#include "locate_array.h"
+
+struct fuzz_line_structure
+{
+  int n_lines;
+  std::vector<double> nu;
+  std::vector<double> El;
+  std::vector<double> gf;
+  std::vector<int>   ion;
+  std::vector<int>   bin;
+};
+
+
+struct AtomicIon
+{
+  int stage;          // ionization stage (0 = neutral, 1 = +, etc..)
+  int ground;         // index of ground state level
+  double chi;         // ionization energy, in eV
+};
+
+struct AtomicLine
+{
+  int lu,ll;           // index of upper/lower level
+  double nu;           // rest frequency (Hz)
+  double f_lu;         // oscillator strength
+  double A_ul;         // Einstein A coeficient
+  double B_ul;         // Einstein B coeficient
+  double B_lu;         // Einstein B coeficient
+  int    bin;          // index of the nu grid bin
+};
+
+struct AtomicLevel
+{
+  int   globalID;       // global id
+  int  ion;             // ionization state (0 = neutral)
+  int   ic;             // index of level we ionize to (-1 if none)
+  int    g;             // statistical weight
+  double E;             // excitation energy above ground (in eV)
+  double E_ion;         // energy to ionize (in eV)
+
+  // photoionization cross-section as a function of wavelength
+  xy_array s_photo;
+  // recombination coefficient as a function of temperature
+  xy_array a_rec;
+
+};
+
+//---------------------------------------------
+// Holds the data for an individual atoms
+//---------------------------------------------
+class IndividualAtomData
+{
+
+public:
+
+  // name of the data file that we read from
+  std::string atom_datafile_;
+
+  // bool to see if data was actually read
+  bool data_exists_;
+
+  AtomicLevel *levels_;      // array of level data
+  AtomicLine  *lines_;       // array of line data
+  AtomicIon   *ions_;        // array of ion data
+  int n_ions_;              // Number of ionic stages considered
+  int n_levels_;            // number of energy levels
+  int n_lines_;             // number of line transitions
+
+  fuzz_line_structure fuzz_lines_; // vector of fuzz lines
+
+  double get_ion_chi(int i) {
+    return ions_[i].chi;
+  }
+  int get_lev_g(int i) {
+    return levels_[i].g;
+  }
+  double get_lev_E(int i) {
+    return levels_[i].E;
+  }
+  int get_lev_ion(int i) {
+    return levels_[i].ion;
+  }
+  int get_lev_ic(int i) {
+    return levels_[i].ic;
+  }
+  double get_lev_Eion(int i) {
+    return levels_[i].E_ion;
+  }
+  double get_lev_photo_cs(int i,double E_ev) {
+    return levels_[i].s_photo.value_at_with_zero_edges(E_ev);
+  }
+  double get_line_nu(int i) {
+    return lines_[i].nu;
+  }
+  double get_line_A(int i) {
+    return lines_[i].A_ul;
+  }
+  double get_line_Bul(int i) {
+    return lines_[i].B_ul;
+  }
+  double get_line_Blu(int i) {
+    return lines_[i].B_lu;
+  }
+  double get_line_f(int i) {
+    return lines_[i].f_lu;
+  }
+  int get_line_l(int i) {
+    return lines_[i].ll;
+  }
+  int get_line_u(int i) {
+    return lines_[i].lu;
+  }
+  int get_line_bin(int i) {
+    return lines_[i].bin;
+  }
+  int get_n_fuzz_lines() {
+    return fuzz_lines_.n_lines;
+  }
+  double get_fuzz_line_nu(int i) {
+    return fuzz_lines_.nu[i];
+  }
+  double get_fuzz_line_gf(int i) {
+    return fuzz_lines_.gf[i];
+  }
+  double get_fuzz_line_El(int i) {
+    return fuzz_lines_.El[i];
+  }
+  int get_fuzz_line_ion(int i) {
+    return fuzz_lines_.ion[i];
+  }
+  int get_fuzz_line_bin(int i) {
+    return fuzz_lines_.bin[i];
+  }
+
+};
+
+
+
+class AtomicData
+{
+
+public:
+
+  // constructor
+  AtomicData();
+  // Destructor
+  ~AtomicData();
+
+  // name of the data file that we read from
+  std::string atom_datafile_;
+
+  // array of structures for the atomic data
+  // of all possible atoms
+  IndividualAtomData atomlist_[MAX_N_ATOMS];
+
+  locate_array nu_grid_;
+
+  int initialize(std::string, locate_array ng);
+  int read_atomic_data(std::string fname, int z);
+  int read_atomic_data(int z);
+  void print();
+  void print_detailed(int);
+
+  int read_fuzzfile_data(std::string fname);
+  int read_fuzzfile_data_for_atom(std::string fname, int);
+
+  IndividualAtomData* get_pointer_to_individual_atom(int z)
+  {
+    return &(atomlist_[z]);
+  }
+
+
+};
+
+
+#endif
