6cd1ac2f5e0d69affab8f57e9684a643097d1acb
Hannah Klion <hklion@berkeley.edu>
2019-12-19 14:21:58 -0800

    Adding particles to escaped list now thread-safe

--- src/transport/transport.cpp
+++ src/transport/transport.cpp
@@ -70,8 +70,9 @@ void transport::step(double dt)
         if (particles[i].type == gammaray)
           gamma_spectrum.count(t_obs,particles[i].nu,particles[i].e,particles[i].D);
         particles[i].t = t_obs;
+#pragma omp critical {
         particles_escaped.push_back(particles[i]);
-        cout << "# Particle escaped" << endl;
+}
       }
   }
 
