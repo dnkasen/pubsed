b31a715b689dcbce07f3145ce029991371ae9cb8
Hannah Klion <hannah.klion@gmail.com>
2020-02-10 16:03:22 -0800

    add analysis defaults file

---
+++ defaults/analysis_defaults.lua
@@ -0,0 +1,15 @@
+spectrum_calc_particle_files = {}
+spectrum_calc_file_to_rank = {}
+spectrum_calc_chk_file = "chk.h5"
+spectrum_calc_out_file = "spectrum_out"
+spectrum_calc_n_ranks_old = 1
+
+spectrum_calc_save_particles = 0
+spectrum_calc_save_particles_file = "particles_out.h5"
+
+spectrum_calc_time_filter = {}
+spectrum_calc_time_phys_filter = {}
+spectrum_calc_energy_filter = {}
+spectrum_calc_velocity_filter = {}
+spectrum_calc_nu_filter = {}
+spectrum_calc_mu_filter = {}
