2a5b0a355f3322d606ac9d45104346ef086454e2
Daniel Kasen <kasen@Witt.local>
2020-03-24 17:55:02 -0700

    Small modifications to script to make toy atoms

--- src/sandbox/nlte_tests/make_atomfile.py
+++ src/sandbox/nlte_tests/make_atomfile.py
@@ -1,6 +1,5 @@
 import h5py
 import pylab as py
-import constants as pc
 
 #############################
 ## make a simple atom
@@ -10,18 +9,18 @@ fname = 'test_atom.hdf5'
 
 # levels
 level_g     = [1,    1,      1,        1,   1]
-level_E     = [0,  0.5,   10.0,     11.0, 0.0]
+level_E     = [0,  1.0,   10.0,     11.0, 0.0]
 level_i     = [0,    0,      0 ,       0,   1]
 
 #ionization energies
-ion_chi     = [E_ion, 999999]
+ion_chi     = [20.0, 999999]
 # index of ground states of all ionization stages
 ion_ground  = [0, 4]
 
 # lines
 lu          = [  1, 3 ,2]
 ll          = [  0 ,2, 0]
-Aij         = [1e9,1e9,1e9]
+Aij         = [1e9,1e9,1]
 
 
 f = h5py.File(fname,'w')
@@ -41,6 +40,11 @@ f.create_dataset(base + "level_i",data = level_i)
 f.create_dataset(base + "line_l",data = ll)
 f.create_dataset(base + "line_u",data = lu)
 f.create_dataset(base + "line_A",data = Aij)
-
-
 f.close()
+
+print ("lines:")
+for i in range(len(lu)):
+    h_ev = 4.135667696e-15
+    dE = level_E[lu[i]] - level_E[ll[i]]
+    nu = dE/h_ev
+    print lu[i],ll[i],dE,nu,Aij[i]
