d914f3dae8203c0fe6ab208eb019926a5d707a45
Daniel Kasen <kasen@berkeley.edu>
2020-11-30 16:24:32 -0800

    add script to convert cmfgen collisional data

---
+++ tools/atomic_data/convert_cmfgen_collisional_data.py
@@ -0,0 +1,116 @@
+import h5py
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+import constants as pc
+
+
+
+
+def add_col(out_fname,lev_fname,col_fname,species,ion,remove_J=True):
+
+    print('\n-----------------')
+    print(species + ' ' + ion)
+    print('-----------------\n\n')
+
+    fout = h5py.File(out_fname,'a')
+    if (not str(species) in fout):
+        species_group = fout.create_group(str(species))
+    ion_group = fout.create_group(str(species) +'/' + str(ion))
+
+    ##########################################
+    ##### read level data
+    fin = open(lev_fname,'r')
+    # read off header
+    line = fin.readline()
+    while ('!Number of transitions' not in line):
+        line = fin.readline()
+    line = fin.readline()
+    line = fin.readline()
+
+    lev_c = []
+    lev_E = []
+    lev_g = []
+
+    invcm_to_ev = 0.00012
+
+    # read level data
+    while (len(line.split()) > 1):
+        data = line.split()
+        config = data[0]
+        if (remove_J):
+            config = re.sub("[\[].*?[\]]", "", config)
+        lev_c.append(config)
+        lev_E.append(float(data[2])*invcm_to_ev)
+        lev_g.append(float(data[1]))
+        line = fin.readline()
+
+    for i in range(len(lev_c)):
+        print(lev_c[i],lev_g[i],lev_E[i])
+
+
+    ######################################
+    ## read collisional data
+    fin = open(col_fname,'r')
+
+    # read off header
+    line = fin.readline()
+    while ('Transition' not in line):
+        line = fin.readline()
+
+    # read temperature array (units = 10^4 K)
+    data = line.split()
+    data.pop(0)
+    for i in range(len(data)):
+        data[i] = data[i].replace('D','e')
+        print(data[i])
+    temps = np.array(data,dtype='d')
+
+    # read data
+    cnt = 0
+    for line in fin:
+
+        line = re.sub(' +-', '-', line)
+        data = line.split()
+        if (len(data) == 0): continue
+        configs = data[0].split('-')
+        if (remove_J):
+            configs[0] = re.sub("[\[].*?[\]]", "", configs[0])
+            configs[1] = re.sub("[\[].*?[\]]", "", configs[1])
+        #configs[1] = configs[1].replace('-','')
+        lev_l = lev_c.index(configs[0])
+        lev_u = lev_c.index(configs[1])
+
+        print(configs[0],configs[1],lev_l,lev_u)
+        data.pop(0)
+        coldata = np.array(data,dtype='d')
+        coldata = 8.63e-08*coldata/temps**0.5*np.exp(-1.967/pc.k_ev/temps/1e4)/5.0
+
+        col_group = fout.create_group(str(species) +'/' + str(ion) + '/' + str(cnt))
+        col_group.create_dataset("T", data=temps*1e4)
+        col_group.create_dataset("C", data=coldata)
+        col_group.create_dataset("lev_l",data=lev_l)
+        col_group.create_dataset("lev_u",data=lev_u)
+
+        cnt += 1
+
+
+out_fname = 'cmfgen_col_data.h5'
+
+col_fname = 'OXY/I/20sep11/oi_col'
+lev_fname = 'OXY/I/20sep11/oi_osc_op'
+species = '8'
+ion     = '0'
+add_col(out_fname,lev_fname,col_fname,species,ion)
+
+col_fname = 'CA/II/30oct12/ca2col.dat'
+lev_fname = 'CA/II/30oct12/ca2_osc.dat'
+species = '20'
+ion     = '1'
+add_col(out_fname,lev_fname,col_fname,species,ion)
+
+col_fname = 'FE/II/10sep16/fe2_col.dat'
+lev_fname = 'FE/II/10sep16/fe2_osc'
+species = '26'
+ion     = '1'
+add_col(out_fname,lev_fname,col_fname,species,ion,remove_J=False)
